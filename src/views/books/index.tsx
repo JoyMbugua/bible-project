c7573987af51bcc8e4978bde69112da06b85f1fc
JoyMbugua <mbuguajoy52@gmail.com>
2023-01-27 20:25:48 +0300

    chapters listing

--- src/views/books/index.tsx
+++ src/views/books/index.tsx
@@ -2,6 +2,7 @@ import { NavLink, useParams } from "react-router-dom"
 import Loader from "../../components/Loader";
 import { useGetChaptersQuery } from "../../store/services/bible"
 import { Chapter } from "../../types";
+import { FixedSizeList } from "react-window";
 
 export default function BibleBook() {
     const { bookId } = useParams()
@@ -15,15 +16,21 @@ export default function BibleBook() {
             <div className="sidebar">
 
                 <nav>
-                    <ul>
-                        {chapters.data.length && chapters.data.map((chap: Chapter) => (
-                            <li key={chap.id}>
-                                <NavLink to={`/chapters/${chap.id}`} className={({ isActive, isPending }) => isActive ? 'active' : isPending ? 'pending' : ''}>
-                                    {chap.reference ? chap.reference : '-'}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
+                    <FixedSizeList height={600} width={200} itemCount={chapters.data.length} itemSize={35} >
+                        {({ index, style }) => {
+                            const chap = chapters.data[index]
+                            return (
+                                <div style={style}>
+                                    <NavLink
+                                        key={chap.id}
+                                        to={`/chapters/${chap.number}`}
+                                        className={({ isPending, isActive }) => isActive ? 'active' : isPending ? 'pending' : ''}>
+                                        {chap.reference}
+                                    </NavLink>
+                                </div>
+                            )
+                        }}
+                    </FixedSizeList>
                 </nav>
             </div>
         </div>
